ec985f00ff4931787e20d2f5e54b90b2aa61272a
ptrotman22 <49842852+ptrotman22@users.noreply.github.com>
2019-04-21 15:40:41 -0400

    Add files via upload
    added webpage files

---
+++ scripts/Homework.js
@@ -0,0 +1,9 @@
+/* Pierce Trotman
+April 14, 2019
+My Function List
+*/
+
+//Thank You alert
+function tyAlert() {
+    alert("Thank you for your interest in our website. We will contact you soon.");
+}
